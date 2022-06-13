56048c8283d5dcc7859a6931c5ad55470f18e4ba
Ricardo Montemayor <ricardomongza@gmail.com>
2022-06-13 13:09:44 -0500

    Connect Adapter

--- app/src/main/java/com/ricardomontemayor/paymeback/LoansListFragment.kt
+++ app/src/main/java/com/ricardomontemayor/paymeback/LoansListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ricardomontemayor.paymeback.databinding.FragmentLoansListBinding
 
 class LoansListFragment : Fragment() {
@@ -21,6 +22,14 @@ class LoansListFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentLoansListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = Adapter(viewModel.loans)
+        binding.rvLoans.adapter = adapter
+        binding.rvLoans.layoutManager = LinearLayoutManager(activity)
     }
 
     override fun onDestroyView() {
