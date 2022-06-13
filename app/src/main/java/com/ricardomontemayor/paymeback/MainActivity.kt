2e7189ab4c092a52e1b575b7c278646495fab609
Ricardo Montemayor <ricardomongza@gmail.com>
2022-06-12 23:51:22 -0500

    Add old code

---
+++ app/src/main/java/com/ricardomontemayor/paymeback/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.ricardomontemayor.paymeback
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.ricardomontemayor.paymeback.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        var navController = navHostFragment.navController
+        setupWithNavController(binding.bottomNavigation, navController)
+    }
+}
