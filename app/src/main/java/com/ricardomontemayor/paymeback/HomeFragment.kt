dbdf2da297dfc35a7373b4eec8e4717b94430ce3
Ricardo Montemayor <ricardomongza@gmail.com>
2022-06-13 12:00:19 -0500

    Add HomeFragment logic

--- app/src/main/java/com/ricardomontemayor/paymeback/HomeFragment.kt
+++ app/src/main/java/com/ricardomontemayor/paymeback/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import com.ricardomontemayor.paymeback.databinding.FragmentHomeBinding
+import java.util.*
 
 class HomeFragment : Fragment() {
 
@@ -25,8 +26,14 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // Add test data
+        viewModel.addLoan("Ricardo", 200.0, Date(), "He owes me")
+        viewModel.addLoan("Luis", 800.0, Date(), "I payed for his concert ticket")
+        viewModel.addLoan("Andres", 2000.0, Date(), "Went for dinner")
 
-
+        // Set total loan amount and count text
+        binding.textViewTotalAmount.text = String.format("$" + viewModel.totalAmount)
+        binding.textViewTotalCount.text = viewModel.totalCount.toString()
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/ricardomontemayor/paymeback/LoanViewModel.kt
+++ app/src/main/java/com/ricardomontemayor/paymeback/LoanViewModel.kt
@@ -6,8 +6,23 @@ import java.util.*
 class LoanViewModel: ViewModel() {
 
     private val _loans = mutableListOf<Loan>()
-
     val loans get() = _loans
+
+    // Total loan amount (sum of all the loans)
+    val totalAmount: Double
+    get() {
+        var sum = 0.0
+        for (loan in loans) {
+            sum += loan.amount
+        }
+        return sum
+    }
+
+    // Total loan count
+    val totalCount: Int
+    get() {
+        return loans.count();
+    }
 
     fun addLoan(name: String, amount: Double, date: Date, concept: String) {
         val newLoan = Loan(name, amount, date, concept)
